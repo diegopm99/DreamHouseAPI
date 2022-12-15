2b1f8377e96af7847ef63c76cc2e01878393c84b
Diego Anfer Perez Mendoza <diegoaperezm.dp@gmail.com>
2022-12-14 23:17:44 -0500

    cambios

--- src/main/java/com/idat/APIDreamHouse/service/RentaServiceImpl.java
+++ src/main/java/com/idat/APIDreamHouse/service/RentaServiceImpl.java
@@ -3,7 +3,6 @@ package com.idat.APIDreamHouse.service;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
